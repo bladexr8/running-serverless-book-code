639d023a8d7ff8306df0d9bef35cd24851388c6a
Stephen Moss <stephen.moss@tquilaanz.com>
2020-05-09 13:20:44 +1000

    add thumbnail generation. Ch10 Finished

--- image-conversion/index.js
+++ image-conversion/index.js
@@ -5,7 +5,9 @@ const path = require('path'),
     extractS3Info = require('./extract-s3-info'),
     silentRemove = require('./silent-remove'),
     OUTPUT_BUCKET = process.env.OUTPUT_BUCKET,
-    supportedFormats = ['jpg', 'jpeg', 'png', 'gif'];
+    supportedFormats = ['jpg', 'jpeg', 'png', 'gif'],
+    THUMB_WIDTH = process.env.THUMB_WIDTH,
+    childProcessPromise = require('./child-process-promise');
 
 exports.handler = async (event, context) => {
     const s3Info = extractS3Info(event),
@@ -14,14 +16,25 @@ exports.handler = async (event, context) => {
         tempFile = path.join(os.tmpdir(), id + extension),
         extensionWithoutDot = extension.slice(1),
         contentType = `image/${extensionWithoutDot}`;
+
     console.log('converting', s3Info.bucket, ':', s3Info.key, 'using', tempFile);
+    
     if (!supportedFormats.includes(extensionWithoutDot)) {
         throw new Error(`unsupported file type ${extension}`);
     }
+    
     // get file from S3 and download into temp file
     await s3Util.downloadFileFromS3(s3Info.bucket, s3Info.key, tempFile);
+
+    // generate the thumbnail using ImageMagick
+    await childProcessPromise.spawn(
+        '/opt/bin/mogrify',
+        ['-thumbnail', `${THUMB_WIDTH}x`, tempFile],
+    );
+    
     // transfer temp file to destination bucket
     await s3Util.uploadFileToS3(OUTPUT_BUCKET, s3Info.key, tempFile, contentType);
+    
     // remove temp file (cleanup)
     await silentRemove(tempFile);
 }
